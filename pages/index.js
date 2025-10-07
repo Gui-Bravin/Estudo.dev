aeb6572eaaa97cb4d68bfab865a680aced173fc0
Guilherme Bravin <81032170+Gui-Bravin@users.noreply.github.com>
2025-10-07 13:11:42 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Amor estou passando por aqui pra te falar que TE AMOOO!!!!</h1>;
+  return <h1>Amor estou passando por aqui pra te falar que TE AMOOO!!!!!!</h1>;
 }
 
 export default Home;
